0fc38abe2b3b81ff817d90ccfac6be798416311f
Ilya Kiselev <ilya.kiselev@hotmail.com>
2024-03-19 00:18:25 -0400

    chore: Adjust format, added params

--- api.py
+++ api.py
@@ -8,15 +8,13 @@ logger = logging.getLogger(__name__)
 
 class PanelAPI:
     def __init__(self, base_url: str, start_port: int = 46000, prefix: str = None):
-        if prefix:
-            self.base_url: str = f"{base_url}/{prefix}"
-        else:
-            self.base_url: str = base_url
+        self.base_url: str = f"{base_url}/{prefix}" if prefix else base_url
         self.start_port: int = start_port
+        self.headers: dict = {}
         logger.info("PanelAPI object has been initialized")
 
-    def auth(self, username: str, password: str):
-        url = f"{self.base_url}/login"
+    def auth(self, username: str, password: str, endoint: str = "/login"):
+        url = f"{self.base_url}{endoint}"
         logger.info(f"Authenticating via {url}")
         body = {
             "username": username,
@@ -31,12 +29,13 @@ class PanelAPI:
         else:
             logger.error(f"Error during auth! Response code: {response.status_code}")
     
-    def get_inbounds(self, endpoint: str = "/xui/API/inbounds/"):
+    def get_inbounds(self, endpoint: str = "/xui/API/inbounds/") -> dict:
         if not "Cookie" in self.headers:
-            logger.error("Authenticate or add a cookie header manually first!")
+            logger.error("Authenticate or add a cookie header manually to the PanelAPI.headers dictionary first!")
             return {'success': False}
         else:
             url = f"{self.base_url}{endpoint}"
+            logger.info("Getting inbounds from {url}")
             response = requests.get(url, headers=self.headers)
 
             if response.status_code == 200:
@@ -50,7 +49,7 @@ class PanelAPI:
                 logger.info(f"Got {response.status_code} from {url}")
                 return {'success': False}
     
-    def create_inbound(self, remark: str, endpoint: str = "/xui/API/inbounds/add", protocol: str = "shadowsocks", password_len: int = 32, enable: bool = True, expiry_time: int = 0):
+    def create_inbound(self, remark: str, endpoint: str = "/xui/API/inbounds/add", protocol: str = "shadowsocks", method = "aes-256-gcm", password_len: int = 32, enable: bool = True, email: str = "", tg_id: str = "", expiry_time: int = 0) -> dict:
         logger.info(f"Adding a new inbound connection for {remark} with {protocol} protocol")
         if not "Cookie" in self.headers:
             logger.error("Authenticate of add a cookie header manually first!")
@@ -76,7 +75,7 @@ class PanelAPI:
                     "enable": enable,
                     "port": port,
                     "protocol": protocol,
-                    "settings": "{{\n  \"method\": \"aes-256-gcm\",\n  \"password\": \"{}\",\n  \"network\": \"tcp,udp\",\n  \"clients\": [\n    {{\n      \"method\": \"aes-256-gcm\",\n      \"password\": \"{}\",\n      \"email\": \"\",\n      \"totalGB\": 0,\n      \"expiryTime\": {},\n      \"enable\": true,\n      \"tgId\": \"\",\n      \"reset\": 0\n    }}\n  ]\n}}".format(password, password, expiry_time)
+                    "settings": f"{{\n  \"method\": \"{method}\",\n  \"password\": \"{password}\",\n  \"network\": \"tcp,udp\",\n  \"clients\": [\n    {{\n      \"method\": \"{method}\",\n      \"password\": \"{password}\",\n      \"email\": \"{email}\",\n      \"totalGB\": 0,\n      \"expiryTime\": {expiry_time},\n      \"enable\": true,\n      \"tgId\": \"{tg_id}\",\n      \"reset\": 0\n    }}\n  ]\n}}"
                 }
                 response = requests.post(url, data=data, headers=headers)
                 if response.status_code == 200 and response.json().get('success') == True:
